0004c40e5c77c1a494784c5fb63fd681144be632
PeterNaggschga <rep.akhcsztan@gmail.com>
2023-05-29 11:18:52 +0200

    added Levels
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

---
+++ src/optimizer/direct/Levels.cpp
@@ -0,0 +1,36 @@
+#include "Levels.h"
+
+level Levels::nextLevel() {
+    currentLevel = static_cast<level>(((int) currentLevel + 1) % 8);
+    return currentLevel;
+}
+
+unsigned long Levels::getRectSubsetSize(unsigned long size) {
+    switch (currentLevel) {
+        case l1_1:
+        case l1_3:
+        case l1_4:
+        case l1_6:
+            return size * 9 / 10 + 1;
+        case l0_2:
+        case l0_5:
+            return size / 10 + 1;
+        default:
+            return size;
+    }
+}
+
+double Levels::getEpsilon() {
+    switch (currentLevel) {
+        case l1_1:
+        case l1_3:
+        case l1_4:
+        case l1_6:
+            return L1_EPSILON;
+        case l0_2:
+        case l0_5:
+            return L0_EPSILON;
+        default:
+            return L2_EPSILON;
+    }
+}
---
+++ src/optimizer/direct/Levels.h
@@ -0,0 +1,32 @@
+#ifndef SIMOPTICON_LEVELS_H
+#define SIMOPTICON_LEVELS_H
+
+
+#include "HyRect.h"
+
+#include <list>
+#include <set>
+
+enum level : unsigned char {
+    l2_0 = 0, l1_1 = 1, l0_2 = 2, l1_3 = 3, l1_4 = 4, l0_5 = 5, l1_6 = 6, l2_7 = 7
+};
+
+class Levels {
+
+private:
+    level currentLevel = l2_0;
+
+public:
+    constexpr static const double L2_EPSILON = 0.00001;
+    constexpr static const double L1_EPSILON = 0.0000001;
+    constexpr static const double L0_EPSILON = 0;
+
+    level nextLevel();
+
+    unsigned long getRectSubsetSize(unsigned long size);
+
+    [[nodiscard]] double getEpsilon();
+};
+
+
+#endif //SIMOPTICON_LEVELS_H
