7d3e20b023f216899d499ba82675ba6cf78adfd8
PeterNaggschga <rep.akhcsztan@gmail.com>
2023-06-05 16:04:58 +0200

    fixed nrThreadRuns
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- src/runner/SimulationRunner.cpp
+++ src/runner/SimulationRunner.cpp
@@ -1,12 +1,14 @@
 #include "SimulationRunner.h"
 
+#include <cmath>
+
 SimulationRunner::SimulationRunner(unsigned int threads, unsigned int runs) : NR_THREADS(threads),
                                                                               NR_RUNS_PER_THREAD(runs) {
 }
 
 map<vector<shared_ptr<Parameter>>, runId, CmpVectorSharedParameter>
 SimulationRunner::runSimulations(set<vector<shared_ptr<Parameter>>, CmpVectorSharedParameter> runs) {
-    unsigned int nrThreadRuns = runs.size() / NR_RUNS_PER_THREAD + 1;
+    unsigned int nrThreadRuns = ceil((double) runs.size() / NR_RUNS_PER_THREAD);
     set<vector<shared_ptr<Parameter>>, CmpVectorSharedParameter> threadRuns[nrThreadRuns];
     for (int i = 0; i < nrThreadRuns; ++i) {
         auto it = runs.begin();
