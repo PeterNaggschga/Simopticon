f6dc300c2573361b1ca4f5c81c36dc4f1115e4ff
PeterNaggschga <rep.akhcsztan@gmail.com>
2023-06-15 12:00:36 +0200

    added execution of controller
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
+#include "controller/Controller.h"
 
-int main() {
+#include <iostream>
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        throw invalid_argument("You need to specify a path to the config.json file as argument!");
+    }
+
+    unique_ptr<Controller> ctr(new Controller(argv[1]));
+    ctr->run();
+
     return 0;
 }
