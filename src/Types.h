1143348226eead1eae7b9ab95c6c0087e3048163
PeterNaggschga <rep.akhcsztan@gmail.com>
2023-07-31 17:03:51 +0200

    added documentation of global headers Types.h and ComparisonFunctions.h
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- src/Types.h
+++ src/Types.h
@@ -3,10 +3,24 @@
 
 #include <string>
 
+/**
+ * In this file types are defined which should be used across the whole framework.
+ */
+
+/**
+ * A floating point type containing the value of an optimized function.
+ */
 typedef long double functionValue;
 
+/**
+ * A floating point type used to represent Parameter values
+ */
 typedef double coordinate;
 
+/**
+ * An identifier that makes different simulation runs in one result file folder distinguishable.
+ * Uniqueness is not being asserted.
+ */
 typedef std::string runId;
 
 #endif //SIMOPTICON_TYPES_H
