c8410a0bbf3d10530952d801f06cd0af6f170f7f
PeterNaggschga <rep.akhcsztan@gmail.com>
2023-07-31 18:37:40 +0200

    added documentation of multithread utilities Multithreaded.h and ThreadsafeQueue.h
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- src/utils/Multithreaded.h
+++ src/utils/Multithreaded.h
@@ -11,20 +11,53 @@
 
 using namespace std;
 
+/**
+ * A class implementing concurrent execution of the same function for different arguments.
+ * The function must be implemented through Multithreaded::work and execution follows the ThreadPool design pattern.
+ * @tparam Key: Argument type of the concurrent Multithreaded::work function.
+ * @tparam T: Result type of the concurrent Multithreaded::work function.
+ * @tparam Compare: Comparison for objects of type Key.
+ * @tparam Allocator: Allocator for pairs of constant Key and T.
+ */
 template<class Key, class T, class Compare = less<Key>, class Allocator = allocator<pair<const Key, T>>>
 class Multithreaded {
 private:
+    /**
+     * Function that should be executed concurrently on different arguments.
+     * @param arg: Argument of the concurrently executed function.
+     * @return Return value of the concurrently executed function.
+     */
     virtual T work(Key arg) = 0;
 
 protected:
+    /**
+     * Maximum number of concurrent threads to be used in ThreadPool.
+     */
     const unsigned int NR_THREADS;
+    /**
+     * ThreadsafeQueue containing the arguments that have to be processed by the ThreadPool.
+     */
     ThreadsafeQueue<Key> queue;
 
+    /**
+     * Pushes given tasks into Multithreaded::queue, creates concurrent threads and merges them when execution is done.
+     * @param runs: Set of arguments on which Multithreaded::work should to be executed.
+     * @return A map which maps arguments to their respective calculated values.
+     */
     virtual map<Key, T, Compare, Allocator> runMultithreadedFunctions(set<Key, Compare> runs);
 
+    /**
+     * Function that is executed by each thread. As long as Multithreaded::queue is not empty, tasks are started.
+     * When Multithreaded::queue is empty, the processed results are returned
+     * @return A map which maps arguments to their respective calculated values.
+     */
     virtual map<Key, T, Compare, Allocator> multithreadFunction();
 
 public:
+    /**
+     * Creates a Multithreaded class that does not use more than the given number of threads.
+     * @param threads: Maximum number of threads to use.
+     */
     explicit Multithreaded(unsigned int threads);
 
 };
--- src/utils/ThreadsafeQueue.h
+++ src/utils/ThreadsafeQueue.h
@@ -7,20 +7,50 @@
 
 using namespace std;
 
+/**
+ * A container class of a queue that is safe for concurrent access of different threads.
+ * @tparam Key: Type of elements in the contained queue.
+ */
 template<class Key>
 class ThreadsafeQueue {
 private:
+    /**
+     * The actual queue data structure.
+     */
     queue<Key> safeQueue;
+    /**
+     * Threadlock to avoid damage to ThreadsafeQueue::safeQueue on concurrent access.
+     */
     mutex queueLock;
+    /**
+     * Number of elements in queue when ThreadsafeQueue::push was called the last time.
+     * Can be used for progress information.
+     */
     size_t startSize = 0;
 
 public:
+    /**
+     * Adds the given value to ThreadsafeQueue::safeQueue.
+     * @param val: Values to be added to queue.
+     */
     void push(Key val);
 
+    /**
+     * Returns the first element of the queue. If the queue is empty, the second entry of the returned pair is false.
+     * @return A pair containing an element of type Key and a boolean determining if access was successful.
+     */
     pair<Key, bool> pop();
 
+    /**
+     * Returns the value of ThreadsafeQueue::startSize.
+     * @return A number representing the number of tasks, when ThreadsafeQueue::push was called last.
+     */
     [[nodiscard]] size_t getStartSize();
 
+    /**
+     * Returns current size of the underlying queue structure.
+     * @return A number representing the size of the queue.
+     */
     [[nodiscard]] size_t getSize();
 
 };
