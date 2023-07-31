5c16d972e1ddbd6ee2d60a5913915497980fd31f
PeterNaggschga <rep.akhcsztan@gmail.com>
2023-07-31 17:04:01 +0200

    added documentation of main.cpp
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,28 @@
 #include <iostream>
 #include <csignal>
 
+/**
+ * Reference to the Controller that is running the optimization.
+ */
 unique_ptr<Controller> ctr;
 
+/**
+ * Handler routine for SIGINT signal which calls Controller::abort and sets the new handler of SIGINT to the default (instant interrupt of the software).
+ * @param s: Necessary parameter for interrupt handlers (unused).
+ * @todo Make interrupt handling independent from OS - currently only Systems using POSIX signals are supported.
+ */
 void interruptHandler([[maybe_unused]] int s) {
     signal(SIGINT, SIG_DFL);
     ctr->abort();
 }
 
+/**
+ * Checks correct command line input and registers interrupt handler for SIGINT signal.
+ * Instantiates Controller or StubController and kicks of the optimization using Controller::run.
+ * @param argc: Number of command line arguments.
+ * @param argv: Array of command line arguments.
+ * @return Status code.
+ */
 int main(int argc, char **argv) {
     if (argc < 2) {
         throw invalid_argument("You need to specify a path to the config.json file as argument!");
