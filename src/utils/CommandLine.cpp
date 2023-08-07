f5ed6691aa08b227a74161bd115064a758a4e756
PeterNaggschga <rep.akhcsztan@gmail.com>
2023-08-07 21:10:29 +0200

    removed using namespace std for CommandLine
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- src/utils/CommandLine.cpp
+++ src/utils/CommandLine.cpp
@@ -9,16 +9,16 @@
 #include <memory>
 #include <stdexcept>
 
-unique_ptr<string> CommandLine::exec(string cmd) {
-    array<char, 128> buffer{};
+std::unique_ptr<std::string> CommandLine::exec(std::string cmd) {
+    std::array<char, 128> buffer{};
     cmd += " 2>&1";
-    unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
     if (!pipe) {
         throw std::runtime_error("Pipeline execution failed!");
     }
-    string result;
+    std::string result;
     while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
         result += buffer.data();
     }
-    return std::make_unique<string>(result);
+    return std::make_unique<std::string>(result);
 }
